756834a7a7075935cac25959a2b0b308efb82e3e
fishwang123 <32293809+fishwang123@users.noreply.github.com>
2018-11-14 19:35:09 +0800

    第四題

--- W610603547/W610603547/Models/book.cs
+++ W610603547/W610603547/Models/book.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace W610603547.Models
 {
-    public class book
+    public class Bookcontext: DbContext
     {
+    }
+    public class Book
+    {
+        public String Bookname { get; set; }
+        public String ISBN { get; set; }
+        public int Price { get; set; }
+
     }
 }
